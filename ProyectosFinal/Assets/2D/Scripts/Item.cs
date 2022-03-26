ebccdc7ce28c7711cea03f3de24e4028cb0de7a8
ThePro365 <alvaro.cancelo@live.u-tad.com>
2022-03-26 20:09:08 +0100

    Funcion Instanciar 1
    sa

---
+++ ProyectosFinal/Assets/2D/Scripts/Item.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public bool clicked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+        if (clicked && Physics.Raycast(castPoint, out hit, Mathf.Infinity, hitLayers))
+        {
+            transform.position = hit.point;
+        }
+    }
+}
